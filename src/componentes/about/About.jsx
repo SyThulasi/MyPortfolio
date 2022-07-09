68a60f1555ee99f19fa1fca1ac0b4bf0a1ce156b
Yogeswaran Thulasiyan <73667144+SyThulasi@users.noreply.github.com>
2022-07-09 23:23:06 +0530

    updated

--- src/componentes/about/About.jsx
+++ src/componentes/about/About.jsx
@@ -123,51 +123,6 @@ const About = () => {
                 </div>
             </section>
 
-            {/*<div className="wrapper" id="Education">*/}
-            {/*    <div className= "education">*/}
-            {/*        <h2>Education</h2>*/}
-            {/*    </div>*/}
-
-            {/*    <section className="education-section">*/}
-            {/*        <div className="center-line">*/}
-            {/*            <a href="#Education" className="scroll-icon"><i className="fas fa-caret-up"></i></a>*/}
-            {/*        </div>*/}
-            {/*        <div className="row row-1">*/}
-            {/*            <section>*/}
-            {/*                <i className="icon fas fa-star"></i>*/}
-            {/*                <div className="details">*/}
-            {/*                    <h3>BSc. in Computer Engineering</h3>*/}
-            {/*                </div>*/}
-            {/*                <div className="details">*/}
-            {/*                    /!*<span>University Of Peradeniya</span>*!/*/}
-            {/*                    <h5>University Of Peradeniya</h5>*/}
-            {/*                </div>*/}
-            {/*                <div className="details">*/}
-            {/*                    <span>2019 - Now</span>*/}
-            {/*                </div>*/}
-            {/*                <p>Current GPA : 3.47/4.00</p>*/}
-            {/*            </section>*/}
-            {/*        </div>*/}
-            {/*        <div className="row row-2">*/}
-            {/*            <section>*/}
-            {/*                <i className="icon fas fa-star"></i>*/}
-            {/*                <div className="details">*/}
-            {/*                    <h3>Physical Science</h3>*/}
-            {/*                </div>*/}
-            {/*                <div className="details">*/}
-            {/*                    /!*<span>University Of Peradeniya</span>*!/*/}
-            {/*                    <h5>J/ Kokuvil Hindu College</h5>*/}
-            {/*                </div>*/}
-            {/*                <div className="details">*/}
-            {/*                    <span>2010 - 2018</span>*/}
-            {/*                </div>*/}
-            {/*                <p>Passed G.C.E.(A/L) examination with 2A’s B</p>*/}
-            {/*            </section>*/}
-            {/*        </div>*/}
-            {/*    </section>*/}
-            {/*    </div>*/}
-
-
 
         </section>
     )
